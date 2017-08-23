564d47b58efdc5c89033e9d2d9ce7569ba84738d
Vaibhav Jain <vaibhav@edcast.com>
2017-08-23 10:23:35 +0530

    using let instead of var

--- js/main.js
+++ js/main.js
@@ -82,18 +82,18 @@ app.controller('mainController', ['$scope', '$http', 'API',function($scope, $htt
 
     this.dataURItoBlob = function(dataURI) {
     // convert base64/URLEncoded data component to raw binary data held in a string
-        var byteString;
+        let byteString;
         if (dataURI.split(',')[0].indexOf('base64') >= 0)
             byteString = atob(dataURI.split(',')[1]);
         else
             byteString = unescape(dataURI.split(',')[1]);
 
         // separate out the mime component
-        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
         // write the bytes of the string to a typed array
-        var ia = new Uint8Array(byteString.length);
-        for (var i = 0; i < byteString.length; i++) {
+        let ia = new Uint8Array(byteString.length);
+        for (let i = 0; i < byteString.length; i++) {
             ia[i] = byteString.charCodeAt(i);
         }
 
@@ -110,7 +110,7 @@ app.controller('mainController', ['$scope', '$http', 'API',function($scope, $htt
       formData.append(original.id, original.files[0], original.id);
 
       Array.prototype.forEach.call(images, function(image) {
-        var blob = _this.dataURItoBlob(image.src);
+        let blob = _this.dataURItoBlob(image.src);
         formData.append(image.id, blob, image.id);
       });
     
